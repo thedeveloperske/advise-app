512da38c84a9c34c2c3e9ee46142491940efdaba
TheDevelopersKE <thedeveloperske@gmail.com>
2023-02-13 22:56:41 +0300

    Pull advise from api

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
     state = { advice : '' };
 
     componentDidMount() {
-        // this.fetchAdvice();
+        this.fetchAdvice();
     }
 
     fetchAdvice = () => {
@@ -29,7 +29,7 @@ class App extends React.Component {
             <div className='app'>
                 <div className='card'>
                     <h1 className='heading'>{advice}</h1>
-                    <button className='button' onClick={this.fetchAdvice()}>
+                    <button className='button' onClick={this.fetchAdvice}>
                         <span>Give Me Advice</span>
                     </button>
                 </div>
